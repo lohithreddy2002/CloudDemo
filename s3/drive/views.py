6faf78c34817aa700d390f3c96f03f2ad85675c6
lohithreddy2002 <rocklohithreddy@gmail.com>
2022-04-24 22:45:04 +0530

    import changes

--- s3/drive/views.py
+++ s3/drive/views.py
@@ -1,15 +1,10 @@
-from ast import Is
 import json
-import re
-from tkinter.tix import STATUS
 from rest_framework.parsers import MultiPartParser, FormParser
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import MediaStorage
 import os
 from django.http import JsonResponse
-from django.contrib.auth import authenticate
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
